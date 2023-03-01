1ebb7cc1d42221532ac4669caa33ef5f0c7dd7b9
Panda <emilianoamaro@hotmail.com>
2023-03-01 11:22:17 -0600

    Practice 2 EA

--- Lab/practica_2.py
+++ Lab/practica_2.py
@@ -1,20 +1,19 @@
-import cv2
+import cv2          #Se cargan las librerias 
 import numpy as np
 import practica_1
 
 
-FILE_NAME = r'fotos\g.png'
+FILE_NAME = r'fotos\D1.png'  #Se carga la imagen 
 
-practica_1.make_image_scale(FILE_NAME, 5)
+practica_1.make_image_scale(FILE_NAME, 5)   #Aqui se coloca que tanto se quiere pixelear la imagen 
+practica_1.get_matrix()                     #En esta parte se muestar la matriz de la imagen 
 try:
-    # Read image from disk.
-    img = cv2.imread('scaled.png')
+   
+    img = cv2.imread('scaled.png')          #Aqui guardamos la imagen escalda o pixeleada anteriormente 
 
-    # Canny edge detection.
-    edges = cv2.Canny(img, 100, 200)
+    edges = cv2.Canny(img, 100, 200)        #Con esta linea logramos que se destaquen los contornos de la imagen 
 
-    # Write image back to disk.
-    cv2.imwrite('result.png', edges)
+    cv2.imwrite('result.png', edges)        #Mostrar la imagen reultante 
 except IOError:
-    print ('Error while reading files !!!')
+    print ('Error !!!')
 
